cd57be72e048277b4fadd9115ff9183b57ce91e1
Ehtishamul-Hassan <hassanarif282@gmail.com>
2023-08-31 18:35:09 +0530

    Some Modification in AtmInterface

--- AtmInterface.java
+++ AtmInterface.java
@@ -21,6 +21,7 @@ class BankAccount {
 class ATM {
     private BankAccount bankAccount;
 
+
     public ATM(BankAccount bankAccount) {
         this.bankAccount = bankAccount;
     }
@@ -33,7 +34,10 @@ class ATM {
             System.out.println("Withdrawal failed: Insufficient balance.");
         } else {
             bankAccount.setAccountBalance(currentBalance - amount);
-            System.out.println("Withdrawn: " + amount);
+            System.out.println("Withdrawn Successful: " + amount);
+            System.out.println();
+            System.out.println("Collect Your Cash");
+            System.out.println("THANK YOU FOR  BANKING WITH US");
         }
     }
 
@@ -44,23 +48,51 @@ class ATM {
             int currentBalance = bankAccount.getAccountBalance();
             bankAccount.setAccountBalance(currentBalance + amount);
             System.out.println("Deposited: " + amount);
+            System.out.println();
+            System.out.println("THANK YOU FOR  BANKING WITH US");
         }
     }
 
     public void checkBalance() {
         int currentBalance = bankAccount.getAccountBalance();
         System.out.println("Account Balance: " + currentBalance);
+        System.out.println();
+        System.out.println("THANK YOU FOR  BANKING WITH US");
     }
 }
 
 public class AtmInterface {
+    public static final String orginalUserId = "Hassan@764";
+    public static final String orginalUserPasscode = "786901";
+
+    //this method check whether the user id and passcode give by the user is correct or not 
+    public static boolean validCredentials(String userId,String passcode) {
+        return userId.equals(orginalUserId) && passcode.equals(orginalUserPasscode);
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Initialize BankAccount with initial balance
-        System.out.print("Enter initial account balance: ");
-        int initialBalance = sc.nextInt();
+        System.out.println("********************************");
+        System.out.println("WELOCME TO THE ATM");
+        System.out.println("********************************");
+
+        // Asking user for the User Id and Passcode
+        System.out.print("Enter your User ID : ");
+        String userId = sc.nextLine();
+        System.out.print("Enter your Passcode : ");
+        String passcode = sc.nextLine();
+
+        // Validate the  Credentials 
+        if(validCredentials(userId, passcode)) {
+            System.out.println("Access Granted");
+            System.out.println();
+
+               // Initialize BankAccount with initial balance
+        int initialBalance = 98533552;
+        System.out.print("Your initial account balance: " + initialBalance);
+        System.out.println();
+        
         BankAccount bankAccount = new BankAccount(initialBalance);
 
         // Initialize ATM with BankAccount
@@ -73,6 +105,7 @@ public class AtmInterface {
             System.out.println("2. Deposit");
             System.out.println("3. Check Balance");
             System.out.println("4. Exit");
+            System.out.print("Enter your choice : ");
             int choice = sc.nextInt();
 
             switch (choice) {
@@ -97,6 +130,10 @@ public class AtmInterface {
                     System.out.println("Invalid choice. Try again.");
                     break;
             }
+        }
+        }
+        else {
+            System.out.println("Access denied. Please enter the correct credentials.");  
         }
     }
 }
